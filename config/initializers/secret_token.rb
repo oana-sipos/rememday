41e868e52df40d8f27fafb2741364c961f405d76
Oana Sipos <oanasipos@gmail.com>
2014-03-09 20:14:02 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rememday::Application.config.secret_key_base = 'cad0db1452e68c8e43e16bd2eb6ed96b6042d4ffbb5e6cf3b93758f554024e8088e9ab4da1bf6f00e315ea49d3a85ee65de00811552e5a2e89143aa5b7707218'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rememday::Application.config.session_store :cookie_store, key: '_rememday_session'
