9faa7f32fb1ff7ef3dd193b5244122bd8d7647ae
Oana Sipos <oana.sipos@up-nxt.com>
2015-09-05 19:32:28 +0200

    View 10 entries/page.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
   # GET /entries
   # GET /entries.json
   def index
-    @entries = Entry.all.paginate(page: params[:page], per_page: 5)
+    @entries = Entry.all.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /entries/1
