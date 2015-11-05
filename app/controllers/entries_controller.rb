c46a222a75080636cbf5ffd41a811fe1ba980754
Oana Sipos <oanasipos@gmail.com>
2015-11-05 21:46:57 +0100

    Order by date, not by id.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
   # GET /entries
   # GET /entries.json
   def index
-    @entries = Entry.all.paginate(page: params[:page], per_page: 10).order('id DESC')
+    @entries = Entry.all.paginate(page: params[:page], per_page: 10).order('date DESC')
   end
 
   # GET /entries/1
