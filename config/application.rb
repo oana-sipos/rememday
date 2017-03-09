3a88c80298b0da0bf3e1a189e0965c14af1832a6
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2017-03-09 10:35:59 +0200

    Really removing react from everywhere

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,5 @@ module Rememday
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
-  config.react.server_renderer_timeout    ||= 20 # seconds
-  config.react.server_renderer = React::ServerRendering::SprocketsRenderer
-  config.react.server_renderer_options = {
-    files: ["react-server.js", "components.js"], # files to load for prerendering
-    replay_console: true,                 # if true, console.* will be replayed client-side
-  }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ Rememday::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.react.variant = :development
-  config.react.addons = true # defaults to false
 end
